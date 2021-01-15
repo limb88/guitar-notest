598f9c3fc4e85f8341a4c8b396261d09a78c1051
limb88 <30526641+limb88@users.noreply.github.com>
2021-01-16 01:13:17 +0700

    Delete js directory

--- js/strings.js
+++
@@ -1,8 +0,0 @@
-var stringsPositions = new Array();
-
-stringsPositions[0]=497;
-stringsPositions[1]=515;
-stringsPositions[2]=533;
-stringsPositions[3]=551;
-stringsPositions[4]=571;
-stringsPositions[5]=589;
