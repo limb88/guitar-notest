d6cfb372aedb18b64ba1bc40d451d4a21cffe435
limb88 <30526641+limb88@users.noreply.github.com>
2021-01-16 01:19:23 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,170 @@
+<!DOCTYPE html>
+<html>
+	<head>
+
+        <?php
+            require_once("parts/index-links.php"); 
+        ?>
+
+
+        <title>GuitarNotes</title>
+
+	</head>
+    <body>
+    
+        <?php 
+            require_once("parts/index-header.php"); 
+        ?>
+        
+        <div class="hero">
+            <div class="hero-left">
+                <h1 class="page-header">Тренажер для&nbsp;запоминания нот на&nbsp;грифе гитары</h1>
+                <h2 class="page-header">Просто тренируйтесь, ноты запомнятся сами</h2>
+                <a class="how-it-works" href="/about.php">Как тренироваться</a>
+            </div>
+            <div class="hero-right">
+                <img src="/img/img-hero.png" />
+            </div>
+        </div>
+
+        <h2 class="page-header">Выберите урок и тренируйтесь:</h2>
+
+        <div id="contBlocks">
+
+            <div id="les-1" class="les blocks">
+            <div class="frets"></div>
+              <h2 style="margin-top:120px;">УРОК 1</h2>
+             <ul class="tips">
+                <li>
+                    Тренируйтесь с&nbsp;гитарой. Старайтесь не&nbsp;смотреть на&nbsp;гриф. Так будут вырабатываться тактильные ощущения.
+                </li>
+                <li>
+                    Во&nbsp;время тренировки проговаривайте название ноты вслух.
+                </li>
+                <li>
+                    Чем ниже нота записана на&nbsp;нотном стане, тем ниже она звучит.
+                </li>
+              </ul>
+              <p>
+                В&nbsp;данном уроке тренируются ноты открытых струн. Урок разбит на&nbsp;2&nbsp;части. Так что вам не&nbsp;составит труда дважды выучить по&nbsp;три ноты.
+              </p>
+              <ul>
+                <li><a href="/lessons/1/1/">часть 1</a>&nbsp;<span>[1-3 струны]</span></li>
+                <li><a href="/lessons/1/2/">часть 2</a>&nbsp;<span>[4-6 струны]</span></li>
+                <li><a href="/lessons/1/3/">часть 3</a>&nbsp;<span>[1-6 струны]</span></li>
+                <li><a href="/files/lesson-1.zip">НОТЫ</a>&nbsp;<span>[gpx, pdf]</span></li>
+              </ul>
+            </div>
+
+            <div id="les-2" class="les blocks">
+            <div class="frets"></div>
+              <h2 style="margin-top:120px;">УРОК 2</h2>
+              <ul class="tips">
+                <li>
+                    Первое время тренируйтесь часто. Если вы&nbsp;не&nbsp;играете с&nbsp;листа, а&nbsp;заучиваете произведения&nbsp;&mdash; ноты будут забываться.
+                </li>
+              </ul>
+              <p>
+                В&nbsp;этом уроке вы&nbsp;будете тренировать ноты, которые расположены на&nbsp;струнах первого, второго и&nbsp;третьего ладов.
+              </p>
+              <ul>
+                <li><a href="/lessons/2/1/">часть 1</a>&nbsp;<span>[1-3 струны]</span></li>
+                <li><a href="/lessons/2/2/">часть 2</a>&nbsp;<span>[1-3 струны #, b]</span></li>
+                <li><a href="/lessons/2/3/">часть 3</a>&nbsp;<span>[1-3 струны, все]</span></li>
+                <li><a href="/lessons/2/4/">часть 4</a>&nbsp;<span>[4-6 струны]</span></li>
+                <li><a href="/lessons/2/5/">часть 5</a>&nbsp;<span>[4-6 струны #, b]</span></li>
+                <li><a href="/lessons/2/6/">часть 6</a>&nbsp;<span>[4-6 струны, все]</span></li>
+                <li><a href="/lessons/2/7/">часть 7</a>&nbsp;<span>[1-6 струны]</span></li>
+                <li><a href="/lessons/2/8/">часть 8</a>&nbsp;<span>[1-6 струны, все]</span></li>
+                <li><a href="/files/lesson-2.zip">НОТЫ</a>&nbsp;<span>[gpx, pdf]</span></li>
+              </ul>
+            </div>
+
+            <div id="les-3" class="les blocks">
+            <div class="frets"></div>
+              <h2 style="margin-top:120px;">УРОК 3</h2>
+              <p>
+                В&nbsp;этой части тренируются ноты, расположенные на&nbsp;четвертом, пятом и&nbsp;шестом ладах. Зная ноты с&nbsp;1&nbsp;по&nbsp;6&nbsp;лады, вы&nbsp;уже сможете играть множество произведений.
+              </p>
+              <ul>
+                <li><a href="/lessons/3/1/">часть 1</a>&nbsp;<span>[1-3 струны]</span></li>
+                <li><a href="/lessons/3/2/">часть 2</a>&nbsp;<span>[1-3 струны #, b]</span></li>
+                <li><a href="/lessons/3/3/">часть 3</a>&nbsp;<span>[1-3 струны, все]</span></li>
+                <li><a href="/lessons/3/4/">часть 4</a>&nbsp;<span>[4-6 струны]</span></li>
+                <li><a href="/lessons/3/5/">часть 5</a>&nbsp;<span>[4-6 струны #, b]</span></li>
+                <li><a href="/lessons/3/6/">часть 6</a>&nbsp;<span>[4-6 струны, все]</span></li>
+                <li><a href="/lessons/3/7/">часть 7</a>&nbsp;<span>[1-6 струны]</span></li>
+                <li><a href="/lessons/3/8/">часть 8</a>&nbsp;<span>[1-6 струны, все]</span></li>
+                <li><a href="/files/lesson-3.zip">НОТЫ</a>&nbsp;<span>[gpx, pdf]</span></li>
+              </ul>
+            </div>
+
+            <div id="les-4" class="les blocks">
+            <div class="frets"></div>
+              <h2 style="margin-top:120px;">УРОК 4</h2>
+              <p>
+                В&nbsp;этой части тренируются ноты, расположенные на&nbsp;четвертом, пятом и&nbsp;шестом ладах. Зная ноты с&nbsp;1&nbsp;по&nbsp;6&nbsp;лады, вы&nbsp;уже сможете играть множество произведений.
+              </p>
+              <ul>
+                <li><a href="/lessons/4/1/">часть 1</a>&nbsp;<span>[1-3 струны]</span></li>
+                <li><a href="/lessons/4/2/">часть 2</a>&nbsp;<span>[1-3 струны #, b]</span></li>
+                <li><a href="/lessons/4/3/">часть 3</a>&nbsp;<span>[1-3 струны, все]</span></li>
+                <li><a href="/lessons/4/4/">часть 4</a>&nbsp;<span>[4-6 струны]</span></li>
+                <li><a href="/lessons/4/5/">часть 5</a>&nbsp;<span>[4-6 струны #, b]</span></li>
+                <li><a href="/lessons/4/6/">часть 6</a>&nbsp;<span>[4-6 струны, все]</span></li>
+                <li><a href="/lessons/4/7/">часть 7</a>&nbsp;<span>[1-6 струны]</span></li>
+                <li><a href="/lessons/4/8/">часть 8</a>&nbsp;<span>[1-6 струны, все]</span></li>
+                <li><a href="/files/lesson-4.zip">НОТЫ</a>&nbsp;<span>[gpx, pdf]</span></li>
+              </ul>
+            </div>
+
+            <div id="les-5" class="les blocks">
+            <div class="frets"></div>
+              <h2 style="margin-top:120px;">УРОК 5</h2>
+              <p>
+                В&nbsp;этой части тренируются ноты, расположенные на&nbsp;четвертом, пятом и&nbsp;шестом ладах. Зная ноты с&nbsp;1&nbsp;по&nbsp;6&nbsp;лады, вы&nbsp;уже сможете играть множество произведений.
+              </p>
+              <ul>
+                <li><a href="/lessons/5/1/">часть 1</a>&nbsp;<span>[1-3 струны]</span></li>
+                <li><a href="/lessons/5/2/">часть 2</a>&nbsp;<span>[1-3 струны #, b]</span></li>
+                <li><a href="/lessons/5/3/">часть 3</a>&nbsp;<span>[1-3 струны, все]</span></li>
+                <li><a href="/lessons/5/4/">часть 4</a>&nbsp;<span>[4-6 струны]</span></li>
+                <li><a href="/lessons/5/5/">часть 5</a>&nbsp;<span>[4-6 струны #, b]</span></li>
+                <li><a href="/lessons/5/6/">часть 6</a>&nbsp;<span>[4-6 струны, все]</span></li>
+                <li><a href="/lessons/5/7/">часть 7</a>&nbsp;<span>[1-6 струны]</span></li>
+                <li><a href="/lessons/5/8/">часть 8</a>&nbsp;<span>[1-6 струны, все]</span></li>
+                <li><a href="/files/lesson-5.zip">НОТЫ</a>&nbsp;<span>[gpx, pdf]</span></li>
+              </ul>
+            </div>
+
+            <div id="les-6" class="les blocks">
+            <div class="frets"></div>
+              <h2 style="margin-top:120px;">УРОК 6</h2>
+              <p>
+                В&nbsp;этой части тренируются ноты, расположенные на&nbsp;четвертом, пятом и&nbsp;шестом ладах. Зная ноты с&nbsp;1&nbsp;по&nbsp;6&nbsp;лады, вы&nbsp;уже сможете играть множество произведений.
+              </p>
+              <ul>
+                <li><a href="/lessons/6/1/">часть 1</a>&nbsp;<span>[1-3 струны]</span></li>
+                <li><a href="/lessons/6/2/">часть 2</a>&nbsp;<span>[1-3 струны #, b]</span></li>
+                <li><a href="/lessons/6/3/">часть 3</a>&nbsp;<span>[1-3 струны, все]</span></li>
+                <li><a href="/lessons/6/4/">часть 4</a>&nbsp;<span>[4-6 струны]</span></li>
+                <li><a href="/lessons/6/5/">часть 5</a>&nbsp;<span>[4-6 струны #, b]</span></li>
+                <li><a href="/lessons/6/6/">часть 6</a>&nbsp;<span>[4-6 струны, все]</span></li>
+                <li><a href="/lessons/6/7/">часть 7</a>&nbsp;<span>[1-6 струны]</span></li>
+                <li><a href="/lessons/6/8/">часть 8</a>&nbsp;<span>[1-6 струны, все]</span></li>
+                <li><a href="/files/lesson-6.zip">НОТЫ</a>&nbsp;<span>[gpx, pdf]</span></li>
+              </ul>
+            </div>
+
+        </div>
+        
+        <?php 
+            require_once("parts/index-footer.php"); 
+        ?>
+
+        <?php 
+            require_once("parts/counters.php"); 
+        ?>
+
+    </body>
+</html>
